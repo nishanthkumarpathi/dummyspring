411c1bb6d296e7865d92da2adfd4fd8444148052
Nishanth Kumar <pathinishanth@gmail.com>
2022-10-03 09:40:51 +0000

    added project

---
+++ demo/src/main/java/com/example/demoapp/demo/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.example.demoapp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Greetings from Spring Boot!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldController.class, args);
+    }
+}
